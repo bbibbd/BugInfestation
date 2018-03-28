1947bb26666691de33f949d40a0f1447bbab9080
bbibbd <bbibochaa74@gmail.com>
2018-03-29 01:34:16 +0900

    Incomplete

--- src/main/java/edu/handong/csee/java/lab06/BugInfestation/BugInfestation.java
+++ src/main/java/edu/handong/csee/java/lab06/BugInfestation/BugInfestation.java
@@ -6,12 +6,39 @@ import java.util.Scanner;	//import the class Scanner
  * User will input the volume of the house, estimated number of roaches in the house.
  * Then the program calculates how long it takes for the roaches to fill the house,
    and how many roaches are inside after that.
- * the output shows the result of summation, subtraction, multiplication of num1 and num2, and  
-   quotient and remains of num1/num2
  * @author KKB
  */
-public class BugInfestation {	//the 
-	final double GROTH_RATE = 0.95;
-	final double ONE_BUG_VOLUME = 0.002;
+public class BugInfestation {	//Declare the class named BugInfestation
+	static final double GROTH_RATE = 0.95;
+	static final double ONE_BUG_VOLUME = 0.002;
 	
+
+	public static void main(String[] args) {
+		Scanner keyboard = new Scanner(System.in);
+		
+		double houseVolume;
+		double population;
+		double totalBugVolume;
+		
+		double startPopulation;
+		double newBugs;
+		double newBugVloume;
+		int countWeeks=0;
+		
+		System.out.print("Enter the total volum of your house in cubic feet: ");
+		houseVolume	= keyboard.nextDouble();
+		System.out.print("Enter the estimated number of roaches in your houuse: ");
+		startPopulation = keyboard.nextDouble();
+		System.out.println("Starting with a roach population of "+population+"\nand a volume of "+houseVolume);
+		
+		population = startPopulation;
+		totalBugVolume = population*ONE_BUG_VOLUME;
+		while(totalBugVolume<houseVolume) {
+			newBugs = population*GROTH_RATE;
+			
+		}
+		
+		
+	
+	}
 }
