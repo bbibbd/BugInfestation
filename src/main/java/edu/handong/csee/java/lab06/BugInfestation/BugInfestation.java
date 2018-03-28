3eb1e5cb0f3e23799ff65d6405773b3c3f04ec28
bbibbd <bbibochaa74@gmail.com>
2018-03-29 00:56:50 +0900

    test!!!!

--- src/main/java/edu/handong/csee/java/lab06/BugInfestation/BugInfestation.java
+++ src/main/java/edu/handong/csee/java/lab06/BugInfestation/BugInfestation.java
@@ -1,7 +1,15 @@
 package edu.handong.csee.java.lab06.BugInfestation;	//the package name and the directory
 import java.util.Scanner;	//import the class Scanner
 
-public class BugInfestation {
+/**
+ * the class named BugInfestation
+ * User will input the volume of the house, estimated number of roaches in the house.
+ * Then the program will calculates how many 
+ * the output shows the result of summation, subtraction, multiplication of num1 and num2, and  
+   quotient and remains of num1/num2
+ * @author KKB
+ */
+public class BugInfestation {	//the 
 	final double GROTH_RATE = 0.95;
 	final double ONE_BUG_VOLUME = 0.002;
 	
