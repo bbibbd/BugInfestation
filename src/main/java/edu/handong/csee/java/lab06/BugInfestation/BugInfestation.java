1cbc6f0926a4bd960c9b75c2a7f897162eacaeb1
bbibbd <bbibochaa74@gmail.com>
2018-03-29 00:46:56 +0900

    test3

--- src/main/java/edu/handong/csee/java/lab06/BugInfestation/BugInfestation.java
+++ src/main/java/edu/handong/csee/java/lab06/BugInfestation/BugInfestation.java
@@ -2,5 +2,7 @@ package edu.handong.csee.java.lab06.BugInfestation;
 import java.util.Scanner;
 
 public class BugInfestation {
-
+	final double GROTH_RATE = 0.95;
+	final double ONE_BUG_VOLUME = 0.002;
+	
 }
